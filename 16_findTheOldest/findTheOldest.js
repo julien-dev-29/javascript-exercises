a28cf4c54ee3f4475408252426985d53cad98bd2
Jurol the Lion <the-lion@example.com>
2025-06-27 10:12:46 +0200

    Finish the exercices

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear()
+    }
+    return death - birth
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const olderAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+        return currentAge > olderAge ? current : oldest
+    })
 };
 
 // Do not edit below this line
