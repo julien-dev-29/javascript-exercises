0490cd3165e17c8da9a79871aa48178d410f79fe
Jurol the Lion <the-lion@example.com>
2025-06-27 09:05:02 +0200

    Finish the calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (/** @type {Array} */arr) {
+  return arr.reduce((total, curr) => total + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, curr) => sum * curr)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum = 1
+  for (let index = 1; index <= num; index++) {
+    sum *= index
+  }
+  return sum
 };
 
 // Do not edit below this line
